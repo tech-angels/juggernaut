ba7d53ba64ae52794892e3965da15cdb7f50be67
Gilbert Roulot <gilbert.roulot@tech-angels.fr>
2011-11-10 11:57:44 +0100

    Add parameters for address and redis
    The address to bind to can now be given with the --address parameter.
    The redis_url parameter now allows to specify which Redis server to use.

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var help = [
     "Starts a juggernaut server using the specified command-line options",
     "",
     "options:",
-    "  --port   PORT       Port that the proxy server should run on",
+    "  --port          PORT       Port that the Juggernaut server should run on",
+    "  --address       ADDRESS    IP   that the Juggernaut server should run on",
+    "  --redis_url     URL        Redis URL for the Redis db the Juggernaut server should use",
     "  --silent            Silence the log output",
     "  -h, --help          You're staring at it"
 ].join('\n');
@@ -17,5 +19,10 @@ if (argv.h || argv.help) {
   return util.puts(help);
 }
 
+if (argv.redis_url) {
+  // Set the REDISTOGO_URL environment to the URL.
+  process.env.REDISTOGO_URL = argv.redis_url
+}
+
 Juggernaut = require("./index");
-Juggernaut.listen(argv.port);
+Juggernaut.listen(argv.port, argv.address);
